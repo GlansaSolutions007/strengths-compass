9bf6ec06a5be3c33d111d4870662b9a8e9918f56
SouravB10 <souravbehuria10@gmail.com>
2025-11-10 10:50:54 +0530

    removed authentication

--- routes/api.php
+++ routes/api.php
@@ -9,9 +9,20 @@ use App\Http\Controllers\Api\QuestionsController;
 use App\Http\Controllers\Api\TestController;
 use Illuminate\Support\Facades\Route;
 
-// Public routes
+// ============================================
+// AUTHENTICATION DISABLED - All routes are public for now
+// To re-enable authentication, wrap routes in: Route::middleware('auth:sanctum')->group(function () { ... });
+// ============================================
+
+// Auth routes (public)
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
+Route::post('logout', [AuthController::class, 'logout']);
+
+// User routes (public for now)
+Route::get('users', [UserController::class, 'index']);
+Route::get('users/{id}', [UserController::class, 'show']);
+Route::put('users/{id}', [UserController::class, 'update']);
 
 // Options routes (public)
 Route::get('options', [OptionsController::class, 'index']);
@@ -20,60 +31,58 @@ Route::get('options/{id}', [OptionsController::class, 'show']);
 // Route::put('options/{id}', [OptionsController::class, 'update']);
 // Route::delete('options/{id}', [OptionsController::class, 'destroy']);
 
-// Protected routes
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('logout', [AuthController::class, 'logout']);
-    Route::get('users', [UserController::class, 'index']);
-    Route::get('users/{id}', [UserController::class, 'show']);
-    Route::put('users/{id}', [UserController::class, 'update']);
+// Cluster CRUD (public for now)
+Route::get('clusters', [ClusterController::class, 'index']);
+Route::get('clusters/{id}', [ClusterController::class, 'show']);
+Route::post('clusters', [ClusterController::class, 'store']);
+Route::put('clusters/{id}', [ClusterController::class, 'update']);
+Route::delete('clusters/{id}', [ClusterController::class, 'destroy']);
 
-    // Cluster CRUD
-    Route::get('clusters', [ClusterController::class, 'index']);
-    Route::get('clusters/{id}', [ClusterController::class, 'show']);
-    Route::post('clusters', [ClusterController::class, 'store']);
-    Route::put('clusters/{id}', [ClusterController::class, 'update']);
-    Route::delete('clusters/{id}', [ClusterController::class, 'destroy']);
+// Construct CRUD (public for now)
+Route::get('constructs', [ConstructController::class, 'index']);
+Route::get('constructs/{id}', [ConstructController::class, 'show']);
+Route::post('constructs', [ConstructController::class, 'store']);
+Route::put('constructs/{id}', [ConstructController::class, 'update']);
+Route::delete('constructs/{id}', [ConstructController::class, 'destroy']);
 
-    // Construct CRUD
-    Route::get('constructs', [ConstructController::class, 'index']);
-    Route::get('constructs/{id}', [ConstructController::class, 'show']);
-    Route::post('constructs', [ConstructController::class, 'store']);
-    Route::put('constructs/{id}', [ConstructController::class, 'update']);
-    Route::delete('constructs/{id}', [ConstructController::class, 'destroy']);
-    
-    // Constructs by Cluster (alternative route)
-    Route::get('clusters/{clusterId}/constructs', [ConstructController::class, 'getByCluster']);
+// Constructs by Cluster (alternative route)
+Route::get('clusters/{clusterId}/constructs', [ConstructController::class, 'getByCluster']);
 
-    // Questions CRUD
-    Route::get('questions', [QuestionsController::class, 'index']);
-    Route::get('questions/{id}', [QuestionsController::class, 'show']);
-    Route::post('questions', [QuestionsController::class, 'store']);
-    Route::put('questions/{id}', [QuestionsController::class, 'update']);
-    Route::delete('questions/{id}', [QuestionsController::class, 'destroy']);
+// Questions CRUD (public for now)
+Route::get('questions', [QuestionsController::class, 'index']);
+Route::get('questions/{id}', [QuestionsController::class, 'show']);
+Route::post('questions', [QuestionsController::class, 'store']);
+Route::put('questions/{id}', [QuestionsController::class, 'update']);
+Route::delete('questions/{id}', [QuestionsController::class, 'destroy']);
 
-    // Questions by Construct
-    Route::get('constructs/{constructId}/questions', [QuestionsController::class, 'byConstruct']);
-    
-    // Questions Bulk Upload
-    Route::post('questions/bulk-upload', [QuestionsController::class, 'bulkUpload']);
+// Questions by Construct
+Route::get('constructs/{constructId}/questions', [QuestionsController::class, 'byConstruct']);
 
-    // Tests CRUD
-    Route::get('tests', [TestController::class, 'index']);
-    Route::get('tests/{id}', [TestController::class, 'show']);
-    Route::post('tests', [TestController::class, 'store']);
-    Route::put('tests/{id}', [TestController::class, 'update']);
-    Route::delete('tests/{id}', [TestController::class, 'destroy']);
-    
-    // Test Clusters Management
-    Route::post('tests/{id}/clusters/attach', [TestController::class, 'attachClusters']);
-    Route::post('tests/{id}/clusters/detach', [TestController::class, 'detachClusters']);
-    
-    // Test Questions and Constructs
-    Route::get('tests/{id}/questions', [TestController::class, 'getQuestions']);
-    Route::get('tests/{id}/constructs', [TestController::class, 'getConstructs']);
-    
-    // Test Question Selection
-    Route::put('tests/{testId}/clusters/{clusterId}/category-counts', [TestController::class, 'setClusterCategoryCounts']);
-    Route::post('tests/{id}/generate-questions', [TestController::class, 'generateQuestionSelection']);
-    Route::post('tests/{id}/regenerate-questions', [TestController::class, 'regenerateQuestionSelection']);
-});
+// Questions Bulk Upload
+Route::post('questions/bulk-upload', [QuestionsController::class, 'bulkUpload']);
+
+// Tests CRUD (public for now)
+Route::get('tests', [TestController::class, 'index']);
+Route::get('tests/{id}', [TestController::class, 'show']);
+Route::post('tests', [TestController::class, 'store']);
+Route::put('tests/{id}', [TestController::class, 'update']);
+Route::delete('tests/{id}', [TestController::class, 'destroy']);
+
+// Test Clusters Management
+Route::post('tests/{id}/clusters/attach', [TestController::class, 'attachClusters']);
+Route::post('tests/{id}/clusters/detach', [TestController::class, 'detachClusters']);
+
+// Test Questions and Constructs
+Route::get('tests/{id}/questions', [TestController::class, 'getQuestions']);
+Route::get('tests/{id}/constructs', [TestController::class, 'getConstructs']);
+
+// Test Question Selection
+Route::put('tests/{testId}/clusters/{clusterId}/category-counts', [TestController::class, 'setClusterCategoryCounts']);
+Route::post('tests/{id}/generate-questions', [TestController::class, 'generateQuestionSelection']);
+Route::post('tests/{id}/regenerate-questions', [TestController::class, 'regenerateQuestionSelection']);
+
+// ============================================
+// TO RE-ENABLE AUTHENTICATION:
+// Wrap the routes above (except register/login) in:
+// Route::middleware('auth:sanctum')->group(function () { ... });
+// ============================================
