a752a2199a1f6431b028e407b82c9f6e765e48d4
SouravB10 <souravbehuria10@gmail.com>
2026-01-19 11:49:36 +0530

    area

--- app/Models/Cluster.php
+++ app/Models/Cluster.php
@@ -11,7 +11,7 @@ class Cluster extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name', 'short_code', 'description', 'age_group_id', 'high_behaviour', 'medium_behaviour', 'low_behaviour', 'is_active', 'is_deleted'];
+    protected $fillable = ['name', 'short_code', 'description', 'area', 'age_group_id', 'high_behaviour', 'medium_behaviour', 'low_behaviour', 'is_active', 'is_deleted'];
 
     public function constructs()
     {
